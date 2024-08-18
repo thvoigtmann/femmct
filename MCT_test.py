0f57a99b5fa3caaa6cf6fe599955de7bcd28be06
Thomas Voigtmann <thomas.voigtmann@dlr.de>
2024-08-18 23:08:01 +0200

    Added channel-with-obstacle mesh.

--- MCT_test.py
+++ MCT_test.py
@@ -9,13 +9,17 @@ path = 'mcttest'
 # channel length and height, and number of elements per direction
 length = 5.
 height = 1.
+cylrad = 0.2
 Nx = 50
 Ny = 50
 
 # initial pressure gradient
 deltap = 2.5
 
-mesh = dolfin.RectangleMesh(dolfin.Point(0.,0.), dolfin.Point(length, height), Nx, Ny, diagonal='crossed')
+#mesh = dolfin.RectangleMesh(dolfin.Point(0.,0.), dolfin.Point(length, height), Nx, Ny, diagonal='crossed')
+mesh = dolfin.Mesh()
+with dolfin.XDMFFile(mesh.mpi_comm(), 'meshdata/channel-sphere.xdmf') as mfile:
+  mfile.read(mesh)
 
 class InFlow(dolfin.SubDomain):
   def inside(self, x, on_boundary):
@@ -29,6 +33,9 @@ class TopWall(dolfin.SubDomain):
 class BottomWall(dolfin.SubDomain):
   def inside(self, x, on_boundary):
     return (on_boundary and dolfin.near(x[1],0.))
+class Cylinder(dolfin.SubDomain):
+  def inside(self, x, on_boundary):
+    return (on_boundary and ((x[0]-1.0)**2 + (x[1]-0.5)**2 < cylrad**2 + dolfin.DOLFIN_EPS))
 
 # values on the inlet shall be overwritten with values on the outlet
 class PeriodicBoundary(dolfin.SubDomain):
@@ -42,6 +49,7 @@ inflow = InFlow()
 outflow = OutFlow()
 topwall = TopWall()
 bottomwall = BottomWall()
+cylinder = Cylinder()
 
 # make a boundaries function that has integer indices for the boundaries
 # that we have defined
@@ -51,12 +59,14 @@ inflow.mark(boundaries, 1)
 outflow.mark(boundaries, 2)
 topwall.mark(boundaries, 3)
 bottomwall.mark(boundaries, 4)
+cylinder.mark(boundaries, 5)
 
 boundary_conditions = {
   1: femmct.p_inlet (deltap),
   2: femmct.p_outlet (0.),
   3: femmct.dirichlet_u ((0.,0.)),
-  4: femmct.dirichlet_u ((0.,0.))
+  4: femmct.dirichlet_u ((0.,0.)),
+  5: femmct.dirichlet_u ((0.,0.))
 }
 
 solver = femmct.StokesSolver(mesh, constrained_domain = PeriodicBoundary(), subdomain_data = boundaries, boundary_conditions = boundary_conditions)
