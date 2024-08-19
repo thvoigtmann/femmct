2ae3c5c8321f6c4307cf1df17cea732ae6f8c6ea
Thomas Voigtmann <thomas.voigtmann@dlr.de>
2024-08-19 16:02:09 +0200

    Small cleanup.

--- MCT_test.py
+++ MCT_test.py
@@ -21,21 +21,26 @@ mesh = dolfin.Mesh()
 with dolfin.XDMFFile(mesh.mpi_comm(), 'meshdata/channel-sphere.xdmf') as mfile:
   mfile.read(mesh)
 
-class InFlow(dolfin.SubDomain):
+class Plane(dolfin.SubDomain):
+  def __init__(self,dim,pos):
+    dolfin.SubDomain.__init__(self)
+    self.dim = dim
+    self.pos = pos
   def inside(self, x, on_boundary):
-    return (on_boundary and dolfin.near(x[0],0.))
-class OutFlow(dolfin.SubDomain):
-  def inside(self, x, on_boundary):
-    return (on_boundary and dolfin.near(x[0],length))
-class TopWall(dolfin.SubDomain):
-  def inside(self, x, on_boundary):
-    return (on_boundary and dolfin.near(x[1],height))
-class BottomWall(dolfin.SubDomain):
-  def inside(self, x, on_boundary):
-    return (on_boundary and dolfin.near(x[1],0.))
+    return (on_boundary and dolfin.near(x[self.dim],self.pos))
 class Cylinder(dolfin.SubDomain):
+  def __init__(self,R,pos):
+    dolfin.SubDomain.__init__(self)
+    self.R = R
+    self.pos = pos
   def inside(self, x, on_boundary):
-    return (on_boundary and ((x[0]-1.0)**2 + (x[1]-0.5)**2 < cylrad**2 + dolfin.DOLFIN_EPS))
+    return (on_boundary and ((x[0]-self.pos[0])**2 + (x[1]-self.pos[1])**2 < self.R**2 + dolfin.DOLFIN_EPS))
+
+inflow = Plane(dim=0,pos=0.)
+outflow = Plane(dim=0,pos=length)
+bottomwall = Plane(dim=1,pos=0.)
+topwall = Plane(dim=1,pos=height)
+cylinder = Cylinder(R=cylrad,pos=[1.0,0.5])
 
 # values on the inlet shall be overwritten with values on the outlet
 class PeriodicBoundary(dolfin.SubDomain):
@@ -44,12 +49,6 @@ class PeriodicBoundary(dolfin.SubDomain):
   def map(self, x, y):
     y[0] = x[0] - length
     y[1] = x[1]
-
-inflow = InFlow()
-outflow = OutFlow()
-topwall = TopWall()
-bottomwall = BottomWall()
-cylinder = Cylinder()
 
 # make a boundaries function that has integer indices for the boundaries
 # that we have defined
