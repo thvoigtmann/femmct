bfc677829b6c4571b9a8465e1817b711666d7d8b
Thomas Voigtmann <thomas.voigtmann@dlr.de>
2024-09-06 13:11:07 +0200

    First attempt preparing for autodoc.
    Using sphinx and readthedocs.

---
+++ docs/conf.py
@@ -0,0 +1,42 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+
+project = 'femmct'
+copyright = '2024, Sebastian Steinhäuser, Timm Treskatis, Thomas Voigtmann'
+author = 'Sebastian Steinhäuser, Timm Treskatis, Thomas Voigtmann'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.intersphinx'
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+
+# options for napoleon extension
+napoleon_google_docstring = False
+napoleon_numpy_docstring = True
+
---
+++ docs/index.rst
@@ -0,0 +1,21 @@
+.. FEM-MCT documentation master file, created by
+   sphinx-quickstart on Fri Sep  6 12:34:04 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+femmct documentation
+====================
+
+This is the beginning of a documentation of the FEM-MCT python code,
+designed to solve incompressible Stokes equations using fenics, combined
+with the mode-coupling theory of the glass transition as an integral
+constitutive equation.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+.. autoclass:: femmct.stokessolver.StokesSolver
+   :members:
+   :inherited-members:
